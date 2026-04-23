5d144837f68891e13f58c83d4d0267fd1ce4645a
Sandra Gajic <66118122+sgajic369@users.noreply.github.com>
2026-04-23 16:44:12 +0200

    Create TaskHandler.py

---
+++ TaskHandler.py
@@ -0,0 +1,10 @@
+class TaskHandler:
+    def __init__(self, TaskName):
+        self.TaskName = TaskName
+
+    def ProcessLogin(self):
+        pass
+
+    def save_to_db(self):
+        print("Saving user...")
+        return 0
